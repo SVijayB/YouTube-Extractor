fee396f2c14c26ce55611b54fa8c6b37a9bb58f7
vijay <vijaybalajitheboss@gmail.com>
2020-06-02 14:47:14 +0530

    Added radio buttons for differnt modes

--- src/YouTube Extractor.py
+++ src/YouTube Extractor.py
@@ -10,26 +10,47 @@ class application():
         self.root.grid_rowconfigure(0,weight=2)
         self.root.grid_columnconfigure(0,weight=1)
         self.root.configure(bg="black")
+
         self.heading = Label(self.root,text = "YOUTUBE EXTRACTOR",fg = "green",
         bg = "black", font=("Castellar", 70))
         self.heading.grid(pady = (0,10))
+
         self.link = Label(self.root,text="Paste the YouTube Link Below", 
         fg = "white", bg = "black", font=("Algerian", 30))
         self.link.grid(pady = (0,20))
+
         self.entryvar = StringVar()
         self.entry = Entry(self.root, width= 70, textvariable = self.entryvar, 
         fg = "green", bg="white", font=("Agency Fb",25))
         self.entry.grid(pady = (0,15), ipady=2)
+
         self.error = Label(self.root, text="", font=("Concert One",20), bg="black", fg="white")
         self.error.grid(pady=(0,8))
+
         self.save = Label(self.root,text="Select The Location You Want To Save In", 
         fg = "white",bg = "black", font=("Algerian",30))
         self.save.grid()
+
         self.directory = Button(self.root, text="Location",
         fg = "white", bg = "black",font = ("Bell MT", 15),command = self.openDirectory)
         self.directory.grid(pady=(10,3))
+
         self.fileLocation = Label(self.root, text="", bg="black")
         self.fileLocation.grid()
+
+        self.choice = Label(self.root,text="Choose The Download Type",
+        bg = "black", fg = "white", font=("Algerian",30))
+        self.choice.grid()
+
+        downloadChoices = [("Video MP4",1),("Audio MP3",2)]
+        self.choiceVar = StringVar()
+        self.choiceVar.set(1)
+
+        for text,mode in downloadChoices:
+            self.type = Radiobutton(self.root, text=text, font=("Northwest old", 15), 
+            fg = "white",bg = "black",variable=self.choiceVar, value = mode)
+            self.type.grid()
+
 
     def openDirectory(self):
         self.FolderName = filedialog.askdirectory()
