c35bba13b05f384715dee5c57284cc2bebaf3dc5
vijay <vijaybalajitheboss@gmail.com>
2020-06-19 17:12:20 +0530

    Moved version, updated icons, added ss

--- src/YouTube Extractor.py
+++ src/YouTube Extractor.py
@@ -4,9 +4,9 @@ from Modules.SelectionWindow import SelectionWindow
 if __name__ == "__main__":
     window = Tk()
     window.state("zoomed")
-    data = open("../version.txt" , "r").read()
+    data = open("../assets/version.txt" , "r").read()
     print("YouTube Extractor | " + data)
     window.title("YouTube Extractor | " + data)
-    window.iconbitmap("../assets/favicon_io/favicon.ico")
+    window.iconbitmap("../assets/Icon.ico")
     app = SelectionWindow(window)
     window.mainloop()
